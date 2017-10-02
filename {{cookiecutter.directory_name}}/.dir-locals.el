cd4e4d659b64e3a38756f33106834f102a592870
Abhinav Sharma <abhi18av@gmail.com>
2017-10-02 13:40:19 +0530

    corrected the .dir-locals file

--- {{cookiecutter.directory_name}}/.dir-locals.el
+++ {{cookiecutter.directory_name}}/.dir-locals.el
@@ -1,6 +1,6 @@
 ((nil . ((cider-refresh-before-fn . "reloaded.repl/suspend")
          (cider-refresh-after-fn  . "reloaded.repl/resume")
          ;(cider-cljs-lein-repl    . "(do (user/go) (user/cljs-repl))")
-         (cider-cljs-lein-repl    . "(do (user/fig-start) (user/nodejs-repl))")))
+         (cider-cljs-lein-repl    . "(do (user/fig-start) (user/nodejs-repl))"))))
 
 ;(async-shell-command "node ")
