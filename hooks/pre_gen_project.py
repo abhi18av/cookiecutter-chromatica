4083fc303c6fef97ac7fc6cb72ba2813900fbbc5
Abhinav Sharma <abhi18av@gmail.com>
2017-10-09 00:52:30 +0530

    working on the pre/post hooks

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,3 +1,5 @@
+# pip3 instal dulwich
+
 from dulwich.repo import Repo
 from dulwich import porcelain
 
@@ -5,3 +7,49 @@ r = Repo(".")
 c = r[r.head()]
 c.message
 porcelain.log(".", max_entries=1)
+
+# pip3 install markdown
+# pip3 install mistune
+
+# pip3 install pyyaml
+
+# pip3 install edn_format
+
+
+# pip3 install toml
+
+# pip3 install csvkit
+
+# pip install Click
+
+# pip install xmltodict
+
+# pip install ptpython 
+
+
+# https://www.codacy.com/
+
+
+https://codecov.io/
+
+https://www.jfrog.com/
+
+https://bintray.com/
+
+
+https://www.ansible.com/
+
+
+https://github.com/bjonnh/PyOrgMode
+
+
+pip install git+https://github.com/hylang/hy.git
+
+# pip install sh
+# pip install delegator.py
+
+
+pip install records
+pip install jrnl
+
+https://github.com/xonsh/xonsh
